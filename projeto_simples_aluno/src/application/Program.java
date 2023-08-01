3bda6954bc32ccdf6ca26f44fbcbfc9b47177624
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-01 15:57:24 -0300

    Procurando e removendo um aluno da lista

--- projeto_simples_aluno/src/application/Program.java
+++ projeto_simples_aluno/src/application/Program.java
@@ -71,13 +71,25 @@ public class Program {
 		for (Aluno aluno : alunos) {
 			
 		if(aluno.getNome().equalsIgnoreCase("alex")) {
+			alunos.remove(aluno);
+			break;
+		}else {
 			System.out.println(aluno); /*Descrição do objeto na memoria*/
 			System.out.println("Média do aluno = " + aluno.getMediaNota());
 			System.out.println(aluno.getAlunoAprovado2());
 			System.out.println("-----------------------------------------------------");
-			break;
-		  }
+		}
 		
+		}
+		
+		for(Aluno aluno : alunos) {
+			System.out.println("Alunos que sobraram na lista:");
+			System.out.println(aluno.getNome());
+			System.out.println("Suas matérias são:");
+			
+			for(Disciplina disciplina : aluno.getDisciplinas()) {
+				System.out.println(disciplina.getDisciplina());
+			}
 		}
 	
 	}
