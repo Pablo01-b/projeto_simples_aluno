f7853c934d652e54955a0bdc281d521392e9f780
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-02 18:06:48 -0300

    Substituindo um aluno na lista

--- projeto_simples_aluno/src/application/Program.java
+++ projeto_simples_aluno/src/application/Program.java
@@ -40,7 +40,7 @@ public class Program {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);*/
 
-		for (int pos = 1; pos <= 4; pos++ ) {
+		for (int pos = 1; pos <= 1; pos++ ) {
 			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+ pos + " ?");
 			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + pos + " ?");
 			Disciplina disciplina = new Disciplina();
@@ -71,6 +71,21 @@ public class Program {
 		for(int pos = 0; pos < alunos.size(); pos++) {
 			
 			Aluno aluno = alunos.get(pos);
+			
+			if(aluno.getNome().equalsIgnoreCase("alex")) {
+				Aluno trocar = new Aluno();
+				trocar.setNome("Aluno foi trocado");
+				
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina("Matemática");
+				disciplina.setNota(96);
+				
+				trocar.getDisciplinas().add(disciplina);
+				
+				alunos.set(pos, trocar);
+				aluno = alunos.get(pos);
+			}
+			
 			System.out.println("Aluno = " + aluno.getNome());
 			System.out.println("Média do aluno = " + aluno.getMediaNota());
 			System.out.println("Resultado = " + aluno.getAlunoAprovado2());
