d91fb2bfccada576039063a32876f514059dbf22
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-23 16:49:08 -0300

    Adicionando o array de notas dentro da disciplina

--- projeto_simples_aluno/src/application/ArrayVetor.java
+++ projeto_simples_aluno/src/application/ArrayVetor.java
@@ -1,21 +1,35 @@
 package application;
 
+import projeto.classes.Aluno;
+import projeto.classes.Disciplina;
 
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 		
-						   
-		String[] valores = new String[4];
+		double[] notas = {8.8, 9.7, 7.6, 6.8};
+		double[] notasLogica = {7.5, 5.6, 9.8, 6.9};
 		
-		valores[0] = "Pablo";
-		valores[1] = "90";
-		valores[2] = "Curso de Java";
-		valores[3] = "contato@gmail.com.br";
+		/*Criação do aluno*/
+		Aluno aluno = new Aluno();
+		aluno.setNome("Pablo");
+		aluno.setNomeEscola("Jdev");
 		
-		for(int pos = 0; pos < valores.length; pos++) {
-			System.out.println("Valor na posição " + pos + " é igual a: " + valores[pos]);
-		}
-	}
+		/*Criação da disciplina*/
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso Java");
+		disciplina.setNota(notas);
+		
+		aluno.getDisciplinas().add(disciplina);
+		
+		/*Criação da disciplina2*/
+		Disciplina disciplina2 = new Disciplina();
+		disciplina.setDisciplina("Lógica de Programação");
+		disciplina2.setNota(notasLogica);
+		
+		aluno.getDisciplinas().add(disciplina2);
+		
 	
+ }
+
 }
