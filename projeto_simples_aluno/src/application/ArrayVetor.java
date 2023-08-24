54c55600d0dc746169cce9445e271aacb9cf6f52
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-24 18:58:46 -0300

    Descobrindo a maior nota do aluno

--- projeto_simples_aluno/src/application/ArrayVetor.java
+++ projeto_simples_aluno/src/application/ArrayVetor.java
@@ -35,12 +35,21 @@ public class ArrayVetor {
 			
 			System.out.println("Disciplina: " + d.getDisciplina());
 			System.out.println("As notas da disciplina são: ");
+			
+			double notaMax = 0.0;
 			for(int pos = 0; pos < d.getNota().length; pos ++) {
 				System.out.println("Nota " + pos + " é igual: " + d.getNota()[pos]);
 				
+				if(pos == 0) {
+					notaMax = d.getNota()[pos];
+				}else {
+					if(d.getNota()[pos] > notaMax) {
+						notaMax = d.getNota()[pos];
+					}
+				}
 			}
+			System.out.println("A maior nota da Disciplina de " + d.getDisciplina() + " e de valor: " + notaMax);
 		}
-	
- }
+	}
 
 }
