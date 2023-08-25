0cf1939427713dcdb7b5f322dc4286564ce5dc70
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-25 16:16:49 -0300

    Lendo uma Matriz multidimensional

--- projeto_simples_aluno/src/application/Matriz.java
+++ projeto_simples_aluno/src/application/Matriz.java
@@ -4,17 +4,24 @@ public class Matriz {
 
 	public static void main(String[] args) {
 		
-		int notas[][] = new int[1][3];
+		int notas[][] = new int[2][3];
 		
 		notas[0][0] = 80;
 		notas[0][1] = 90;
 		notas[0][2] = 70;
-		notas[1][3] = 90;
 		
-		System.out.println(notas[0][0]);
-		System.out.println(notas[0][1]);
-		System.out.println(notas[0][2]);
-
+		notas[1][0] = 40;
+		notas[1][1] = 60;
+		notas[1][2] = 30;
+		
+		/*Percorre as linhas*/
+		for(int posLinha = 0; posLinha < notas.length; posLinha++) {
+			System.out.println("==================================================");
+			/*Para cada linha Percorrer as colunas ARRAY*/
+			for(int posColuna = 0; posColuna < notas[posLinha].length; posColuna++) {
+				System.out.println("Valor da matriz: " + notas[posLinha][posColuna]);
+			}
+		}
 	}
 
 }
